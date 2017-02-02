5e681f191cf5938fa7e3c3387cfb5dc48c34dfc2
jamiegill <jamiegill.03@gmail.com>
2017-02-02 03:28:02 +0000

    jinja template usage

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from os import environ
 
 app = Flask(__name__)
@@ -10,31 +10,16 @@ def say_hi():
     
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-    <h1>
-            Hello {}!
-        <h1>
-        <p>
-            Here's a picture of a kitten. Awww...
-        </p>
-        <img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg">
-    """
-    return html.format(name.title())
+    return render_template('hello_template.html',
+                            name=name)
     
 @app.route("/hello/jedi/<first_name>/<last_name>")
 def jedi_person(first_name, last_name):
     first_name = first_name[0:2]
     last_name = last_name[0:3]
-    html = """
-    <h1>
-        What is your Jedi name?
-    </h1>
-    <p> 
-        Your jedi name is "{}{}"
-    </p>
-    <img src="https://images.unsplash.com/profile-fb-1465355193-c3fa695c1fda.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=48c018887476054afc6edccbc5e5fa30">
-    """
-    return html.format(last_name, first_name)
+    return render_template('jedi_template.html',
+                            last_name=last_name,
+                            first_name=first_name)
     
         
 if __name__ == "__main__":
